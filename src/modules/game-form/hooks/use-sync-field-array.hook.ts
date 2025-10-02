a854a28a270cd415dee352f50dc02c1b8df5d67d
TimPulin <tigeronsun@gmail.com>
2025-10-02 09:38:41 +0300

    fix StrictMode rerender

--- src/modules/game-form/hooks/use-sync-field-array.hook.ts
+++ src/modules/game-form/hooks/use-sync-field-array.hook.ts
@@ -1,6 +1,6 @@
 import type { FieldArrayComponentProps } from "../types/field-array-component-props.type.ts";
 import { useFieldArray } from "react-hook-form";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export function useSyncFieldArray(props: FieldArrayComponentProps) {
   const { name, amount, formHook, defaultValue } = props;
@@ -9,13 +9,23 @@ export function useSyncFieldArray(props: FieldArrayComponentProps) {
     control: formHook.control,
   });
 
+  const initialized = useRef(false);
+
   useEffect(() => {
-    if (amount > fields.length) {
+    if (initialized.current && amount > fields.length) {
       for (let i = fields.length; i < amount; i++) {
         append(defaultValue);
       }
     }
-  }, [amount, fields, append]);
+
+    initialized.current = true;
+
+    if (fields.length === 0 && amount > 0) {
+      for (let i = 0; i < amount; i++) {
+        append(defaultValue ?? { checked: false });
+      }
+    }
+  }, [amount, fields]);
 
   return fields;
 }
