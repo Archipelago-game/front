c1825aab566a817b1042f286597a27e27dccde69
gen <kyliki@yandex.ru>
2025-10-10 03:13:22 +0300

    coordination

--- src/modules/game-form/ui/sections/attributes/coordination/CoordinationSkillTableAuto.tsx
+++ src/modules/game-form/ui/sections/attributes/coordination/CoordinationSkillTableAuto.tsx
@@ -1,6 +1,6 @@
 import { useCustomFormContext } from "../../../../providers/use-custom-context-form.hook.ts";
-import { buildSkillGroups } from "../skill-table/build-skill-groups.util.ts";
-import SkillsTable from "../skill-table/SkillsTable.tsx";
+
+import SkillsTable, { type SkillGroup } from "../skill-table/SkillsTable.tsx";
 
 export default function CoordinationSkillTableAuto() {
   const { values } = useCustomFormContext();
@@ -8,8 +8,57 @@ export default function CoordinationSkillTableAuto() {
   if (!values) return null;
 
   const coordination = values.stats.coordination;
+  const coordinationSkillGroups: SkillGroup<string>[] = [
+    {
+      name: coordination.firearms.name,
+      expertiseFieldName: "stats.coordination.firearms.expertise",
+      OZFieldName: "stats.coordination.firearms.OZ",
+      skills: [
+        {
+          ...coordination.firearms.skills.pistols,
+          fieldName: "stats.coordination.firearms.skills.pistols.focus",
+        },
+        {
+          ...coordination.firearms.skills.arquebuses,
+          fieldName: "stats.coordination.firearms.skills.arquebuses.focus",
+        },
+        {
+          ...coordination.firearms.skills.fieldsQueen,
+          fieldName: "stats.coordination.firearms.skills.fieldsQueen.focus",
+        },
+      ],
+    },
+    {
+      name: coordination.seafaring.name,
+      expertiseFieldName: "stats.coordination.seafaring.expertise",
+      OZFieldName: "stats.coordination.seafaring.OZ",
+      skills: [
+        {
+          ...coordination.seafaring.skills.helmsman,
+          fieldName: "stats.coordination.seafaring.skills.helmsman.focus",
+        },
+        {
+          ...coordination.seafaring.skills.boatswain,
+          fieldName: "stats.coordination.seafaring.skills.boatswain.focus",
+        },
+      ],
+    },
+    {
+      name: coordination.defense.name,
+      expertiseFieldName: "stats.coordination.defense.expertise",
+      OZFieldName: "stats.coordination.defense.OZ",
+      skills: [
+        {
+          ...coordination.defense.skills.parry,
+          fieldName: "stats.coordination.defense.skills.parry.focus",
+        },
+        {
+          ...coordination.defense.skills.cover,
+          fieldName: "stats.coordination.defense.skills.cover.focus",
+        },
+      ],
+    },
+  ];
 
-  const groups = buildSkillGroups("coordination", coordination);
-
-  return <SkillsTable skillGroups={groups} />;
+  return <SkillsTable skillGroups={coordinationSkillGroups} />;
 }
