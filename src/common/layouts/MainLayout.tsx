5621aeae3bd43f5599133bce27cf2bc222a327d1
gen <kyliki@yandex.ru>
2025-10-16 22:50:15 +0300

    add icon to Header

--- src/common/layouts/MainLayout.tsx
+++ src/common/layouts/MainLayout.tsx
@@ -1,9 +1,11 @@
 import { Container } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import Header from "../../modules/header/Header.tsx";
 
 export default function MainLayout() {
   return (
     <Container maxWidth={"xl"} sx={{ px: 2, paddingBottom: 2 }}>
+      <Header />
       <Outlet />
     </Container>
   );
