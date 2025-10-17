899256f1074b68f9a564b005e5f24542088855d2
TimPulin <tigeronsun@gmail.com>
2025-10-17 09:29:14 +0300

    get and save token, user-info

--- src/api/backendless-config.ts
+++ src/api/backendless-config.ts
@@ -23,6 +23,7 @@ export const oauthApi = {
         },
         body: JSON.stringify({
           redirectAfterLoginUrl,
+          scopes: ["email", "https://www.googleapis.com/auth/userinfo.profile"],
         }),
       },
     );
