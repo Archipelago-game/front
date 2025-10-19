2b3332232b5c4734c5c4bea7c3ea201c5fc22d93
Pavel Pulin <pulin.pavel@gmail.com>
2025-10-19 13:27:22 +0300

    fix auth user (#45)
    * fix auth user
    
    * fix linter
    
    ---------
    
    Co-authored-by: Pavel Pulin <pavel.pulin@vallettasoftware.com>

--- src/api/local-storage.ts
+++ src/api/local-storage.ts
@@ -2,6 +2,8 @@ import { FORM_DEFAULT_VALUES } from "../modules/game-form/consts/form-default-va
 import type { FormType } from "../modules/game-form/types/form/form.type.ts";
 
 const STORAGE_KEY = "ARCHIPELAGO";
+const USER_TOKEN_KEY = "USER_TOKEN";
+const USER_ID_KEY = "USER_ID";
 
 export function getLocalStorage() {
   const data = localStorage.getItem(STORAGE_KEY);
@@ -14,4 +16,26 @@ export function getLocalStorage() {
 
 export function setLocalStorage(data: FormType) {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
+
+export function setTokenLocalStorage(token: string | null) {
+  if (!token) {
+    localStorage.removeItem(USER_TOKEN_KEY);
+  }
+  localStorage.setItem(USER_TOKEN_KEY, token || "");
+}
+
+export function getTokenLocalStorage() {
+  localStorage.getItem(USER_TOKEN_KEY);
+}
+
+export function setUserIdLocalStorage(userId: string | null) {
+  if (!userId) {
+    localStorage.removeItem(USER_ID_KEY);
+  }
+  localStorage.setItem(USER_ID_KEY, userId || "");
+}
+
+export function getUserIdLocalStorage() {
+  localStorage.getItem(USER_ID_KEY);
 }
