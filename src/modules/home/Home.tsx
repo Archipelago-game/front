b8f54fbe45db502552a6e1ce8862d32e985373fd
gen <kyliki@yandex.ru>
2025-11-10 22:41:58 +0300

    Home styles

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -5,18 +5,25 @@ import AnimatedSvg from "../screen-saver/ScreenSaver.tsx";
 import { useState } from "react";
 import Prelude from "./Prelude";
 import AuthorizationButton from "../../common/components/auth-button/AuthorizationButton.tsx";
+import { useAuthContext } from "../../app/providers/auth-provider/use-auth-context.hook.ts";
 
 export default function Home() {
   const [isShowScreenSaver, setShowScreenSaver] = useState(true);
+  const { userInfo } = useAuthContext();
 
   return (
     <Box
       sx={{
         display: "flex",
         flexDirection: "column",
+        gap: "10px",
       }}
     >
-      <AuthorizationButton />
+      {!userInfo && (
+        <Box textAlign="center">
+          <AuthorizationButton />
+        </Box>
+      )}
       <Button component={RouterLink} to={"/characters"}>
         Персонажи
       </Button>
