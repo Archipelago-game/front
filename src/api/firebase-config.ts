a29debd39952d5a47fec6c116ae7e245add96e37
TimPulin <tigeronsun@gmail.com>
2025-11-12 07:18:53 +0300

    firebase config initializeFirestore

--- src/api/firebase-config.ts
+++ src/api/firebase-config.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore, persistentLocalCache } from "firebase/firestore";
 
 // Firebase configuration
 const firebaseConfig = {
@@ -18,7 +18,9 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase services
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = initializeFirestore(app, {
+  localCache: persistentLocalCache(),
+});
 
 // Configure Google Auth Provider
 export const googleProvider = new GoogleAuthProvider();
