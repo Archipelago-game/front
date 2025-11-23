a049db52b301c4fadd54d258bf09654a36889ad2
gen <kyliki@yandex.ru>
2025-11-23 13:08:30 +0300

    useFormDialogContext

--- src/modules/form-dialog/use-form-dialog.hook.ts
+++ src/modules/form-dialog/use-form-dialog.hook.ts
@@ -1,3 +1,17 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
+import type { FormDialogProps } from "./FormDialog.tsx";
 
-const FormDialogContext = createContext();
+export interface FormDialogType {
+  open: (data: Omit<FormDialogProps, "isOpen">) => void;
+}
+
+export const FormDialogContext = createContext<FormDialogType | null>(null);
+
+export function useFormDialogContext() {
+  const context = useContext(FormDialogContext);
+  if (!context)
+    throw new Error(
+      "useFormDialogContext must be used as useFormDialogContext",
+    );
+  return context;
+}
