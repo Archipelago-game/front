47e36dd88a5b3be1617b1744657782dd76de55e0
TimPulin <tigeronsun@gmail.com>
2025-11-25 20:01:55 +0300

    immortal experience

--- src/modules/game-form/ui/sections/experience/Experience.tsx
+++ src/modules/game-form/ui/sections/experience/Experience.tsx
@@ -2,36 +2,66 @@ import CustomLabel from "../../components/CustomLabel.tsx";
 
 import { Box, useMediaQuery } from "@mui/material";
 import BaseField from "../../components/BaseField.tsx";
+import { useWatchImmortal } from "../base-info/use-watch-immortal.hook.ts";
+
+const styles = {
+  display: "flex",
+  flexWrap: "wrap",
+  gap: "0.2em",
+};
 
 export default function Experience() {
   const isBelow530 = useMediaQuery("(max-width: 530px)");
+
+  const isImmortal = useWatchImmortal();
   return (
     <CustomLabel label={{ text: "Опыт" }} sx={{ flex: "1 1 1" }}>
       <Box
         sx={{
-          display: "flex",
-          flexWrap: "wrap",
-          gap: "0.2em",
           width: isBelow530 ? "100%" : "300px",
         }}
       >
-        <BaseField
-          fieldName="experience.total"
-          label={{
-            color: "secondary",
-            text: "Всего",
-          }}
-          orientation="row"
-        />
+        <Box sx={{ ...styles }}>
+          <BaseField
+            fieldName="experience.total"
+            label={{
+              color: "secondary",
+              text: "Всего",
+            }}
+            orientation="row"
+          />
 
-        <BaseField
-          fieldName="experience.used"
-          label={{
-            color: "secondary",
-            text: "Исп.",
-          }}
-          orientation="row"
-        />
+          <BaseField
+            fieldName="experience.used"
+            label={{
+              color: "secondary",
+              text: "Исп.",
+            }}
+            orientation="row"
+          />
+        </Box>
+
+        {isImmortal && (
+          <Box sx={{ ...styles }}>
+            <BaseField
+              fieldName="immortal.experience.salted"
+              label={{
+                color: "secondary",
+                text: "Засоленный",
+              }}
+              orientation="row"
+            />
+
+            <BaseField
+              fieldName="immortal.experience.deferred"
+              label={{
+                color: "secondary",
+                text: "Отложенный",
+              }}
+              orientation="row"
+            />
+          </Box>
+        )}
       </Box>
     </CustomLabel>
   );
