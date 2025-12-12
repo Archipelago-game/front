6d13cf95e6c2457242b04fb42f3c167e79657265
Pavel Pulin <pulin.pavel@gmail.com>
2025-12-12 17:31:11 +0200

    change icon (#97)
    Co-authored-by: Pavel Pulin <pavel.pulin@vallettasoftware.com>

--- src/common/components/icons/DropIcon.tsx
+++ src/common/components/icons/DropIcon.tsx
@@ -1,4 +1,4 @@
-import { SvgIcon } from "@mui/material";
+import HeartBrokenIcon from "@mui/icons-material/HeartBroken";
 
 // #2196f3
 
@@ -7,11 +7,8 @@ interface CustomIconProps {
 }
 export default function DropIcon({ color = "#f44336" }: CustomIconProps) {
   return (
-    <SvgIcon sx={{ color: color, width: "100%", height: "auto" }}>
-      <path
-        d="M12,4 C15,4 18,7 18,12 C18,17 12,22 12,22 C12,22 6,17 6,12 C6,7 9,4 12,4 Z"
-        fill="currentColor"
-      />
-    </SvgIcon>
+    <>
+      <HeartBrokenIcon sx={{ color }} />
+    </>
   );
 }
