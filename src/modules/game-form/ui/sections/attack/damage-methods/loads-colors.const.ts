ed312ccc53d54c445cbb9a0f777e31404e59faec
Pavel Pulin <pavel.pulin@vallettasoftware.com>
2025-12-16 17:28:04 +0200

    fix sty;es

--- src/modules/game-form/ui/sections/attack/damage-methods/loads-colors.const.ts
+++ src/modules/game-form/ui/sections/attack/damage-methods/loads-colors.const.ts
@@ -1,4 +1,4 @@
-import type { StatementColorMapping } from "../../../../components/check-icon-box/check-icon-box.type.ts";
+import type { StatementColorMapping } from "../../../components/check-icon-box/check-icon-box.type.ts";
 
 /**
  * Цветовая схема для чекбоксов зарядов оружия
@@ -6,6 +6,6 @@ import type { StatementColorMapping } from "../../../../components/check-icon-bo
  * - false (неотмеченное) - серый цвет, символизирует использованный заряд
  */
 export const LOADS_STATEMENT_COLOR_MAP: StatementColorMapping = new Map([
-  [true, "#43a047"],  // Material Green 600 - отмеченное (заряд доступен)
-  [false, "#666"],    // Серый - неотмеченное (заряд использован)
+  [true, "#43a047"], // Material Green 600 - отмеченное (заряд доступен)
+  [false, "#666"], // Серый - неотмеченное (заряд использован)
 ]);
