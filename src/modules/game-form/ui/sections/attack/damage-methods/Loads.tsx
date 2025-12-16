200752f0b5234e04ca2b34e27cd8b07f3e36ed67
Pavel Pulin <pavel.pulin@vallettasoftware.com>
2025-12-16 17:25:27 +0200

    shots

--- src/modules/game-form/ui/sections/attack/damage-methods/Loads.tsx
+++ src/modules/game-form/ui/sections/attack/damage-methods/Loads.tsx
@@ -1,10 +1,12 @@
 import { useSyncFieldArray } from "../../../../hooks/use-sync-field-array.hook.ts";
 import { Controller } from "react-hook-form";
 import { Box, type CheckboxProps } from "@mui/material";
+import CelebrationIcon from "@mui/icons-material/Celebration";
 import CustomLabel from "../../../components/CustomLabel.tsx";
 import { useCustomFormContext } from "../../../../providers/use-custom-context-form.hook.ts";
 import type { FieldArrayComponentShortProps } from "../../../../types/field-array-component-props.type.ts";
 import CheckIconBox from "../../../components/check-icon-box/CheckIconBox.tsx";
+import { LOADS_STATEMENT_COLOR_MAP } from "./loads-colors.const.ts";
 
 interface Props extends FieldArrayComponentShortProps {
   size?: CheckboxProps["size"];
@@ -51,13 +53,13 @@ export default function Loads(props: Props) {
             name={`attack.methods.list.${props.index}.loads.list.${index}.checked`}
             control={methods.control}
             render={({ field }) => (
-              // note для отображения иконки вместо checkbox, добавить пропу Icon c требуемой иконкой
-              // todo addIcon
               <CheckIconBox
                 field={field}
                 onChange={(e) => {
                   onChange(field, e);
                 }}
+                Icon={CelebrationIcon}
+                colors={LOADS_STATEMENT_COLOR_MAP}
               />
             )}
           />
